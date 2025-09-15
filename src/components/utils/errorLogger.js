4f9b8731e451e41ed75eb5d4a233ca188195960d
Taiye Goodness <taiyegoodness@gmail.com>
2025-09-15 17:57:29 +0100

    error log added

--- src/components/utils/errorLogger.js
+++ src/components/utils/errorLogger.js
@@ -1,7 +1,7 @@
 export const logErrorToTelegram = async (error, context = "General") => {
-  const BOT_TOKEN = "7831671379:AAEgkMTbrECnJEnzpz25VwsvvUYW610CSOE";
-  const CHAT_ID = "-1002572086216";
-
+  const BOT_TOKEN = process.env.REACT_APP_TELEGRAM_BOT_TOKEN;
+  const CHAT_ID = process.env.REACT_APP_TELEGRAM_CHAT_ID;
+  
   // Detect the page where the error happened
   const page = typeof window !== "undefined" ? window.location.pathname : "Unknown";
 
