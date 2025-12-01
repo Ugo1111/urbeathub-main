6620b64b9c68feef2afd3ccac893aadfb0cda186
Tayexy <taiyegoodness@gmail.com>
2025-12-01 11:28:57 +0100

    added prender on firebase

--- functions/index.js
+++ functions/index.js
@@ -35,3 +35,23 @@ exports.initializePaystackPayment = initializePaystackPayment;
 
 const { paystackWebhook } = require("./paystackWebhook");
 exports.paystackWebhook = paystackWebhook;
+
+
+// 🔥 Prerender function export
+exports.prerender = functions.https.onRequest(async (req, res) => {
+  const fetch = require("node-fetch");
+  const PRERENDER_TOKEN = process.env.PRERENDER_TOKEN || "YOUR_PRERENDER_TOKEN_HERE";
+
+  try {
+    const prerenderUrl = `https://service.prerender.io${req.url}`;
+    const prerenderRes = await fetch(prerenderUrl, {
+      headers: { "X-Prerender-Token": PRERENDER_TOKEN },
+    });
+    const html = await prerenderRes.text();
+    res.set("Cache-Control", "public, max-age=3600");
+    res.status(prerenderRes.status).send(html);
+  } catch (err) {
+    console.error("Prerender error:", err);
+    res.status(500).send("Prerender failed");
+  }
+});
